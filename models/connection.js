4c35cbdbeeb6f67840ae6ab495204c503b5b1f02
Thomas Lebel <thomas.lebel38@gmail.com>
2025-01-16 10:39:31 +0100

    Connexion BDD

---
+++ models/connection.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+// Pour récupérer la connection string à l'intérieur du .env
+const connectionString = process.env.CONNECTION_STRING; 
+
+mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
+ .then(() => console.log('Database connected'))
+ .catch(error => console.error(error));
